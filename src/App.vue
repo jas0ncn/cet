880124c1861f074c45beb837f0671ecd58218b6d
Jas0ncn <ceocjy@vip.qq.com>
2016-08-18 17:30:09 +0800

    release

--- src/App.vue
+++ src/App.vue
@@ -114,250 +114,244 @@ export default {
 <style>
 body {
     background: #FFF;
+    max-width: 600px;
+    margin: 0 auto;
 }
 </style>
 
 <style lang="less" scoped>
 
+body {
+    padding: 0;
+}
+
 header {
+    height: 100px;
+    color: #FFF;
+    font-size: 28px;
+    text-shadow: 0 0 5px rgba(0,0,0,.4);
+    line-height: 100px;
+    background-image: url('./assets/bg.jpg');
+    background-size: 100% 100%;
+    text-align: center;
+
+    span {
+        display: none;
+    }
+}
+
+.warn {
+    position: absolute;
+    left: 0;
+    top: 0;
     width: 100%;
-    height: 160px;
-    border-bottom: 1px solid #eee;
-    font-size: 42px;
+    height: 40px;
+    background: #F45757;
+    color: #FFF;
+    text-align: center;
+    transition: 300ms ease;
+    transform: translateY(-100%);
+
+    p {
+        font-size: 14px;
+        font-weight: bold;
+        line-height: 40px;
+    }
 }
 
-@media screen and (max-width: 450px) {
-    body {
-        padding: 0;
-    }
+.warning {
+    transform: translateY(0);
+}
 
-    header {
-        height: 100px;
-        color: #FFF;
-        font-size: 28px;
-        text-shadow: 0 0 5px rgba(0,0,0,.4);
-        line-height: 100px;
-        background-image: url('./assets/bg.jpg');
-        background-size: 100% 100%;
-        text-align: center;
+.mobileTips {
+    width: 94%;
+    margin-left: 3%;
+    margin-top: 10px;
+    padding: 10px;
+    color: #333;
+    background: #F8F8F8;
+    border-radius: 6px;
+    border: 1px solid #eee;
 
-        span {
-            display: none;
+    p {
+        font-size: 13px;
+        line-height: 18px;
+        text-indent: 2em;
+        text-align: justify;
+
+        a {
+            color: blue;
+            text-decoration: underline;
+        }
+
+        b {
+            color: #F45757;
         }
     }
+}
 
-    .warn {
-        position: absolute;
-        left: 0;
-        top: 0;
+.form {
+    width: 94%;
+    margin-left: 3%;
+    margin-top: 10px;
+    padding: 0 10px;
+    color: #222;
+
+    label {
+        font-size: 14px;
+        line-height: 20px;
+    }
+
+    input[type="text"] {
         width: 100%;
-        height: 40px;
-        background: #F45757;
-        color: #FFF;
-        text-align: center;
-        transition: 300ms ease;
-        transform: translateY(-100%);
-
-        p {
-            font-size: 14px;
-            font-weight: bold;
-            line-height: 40px;
-        }
-    }
-
-    .warning {
-        transform: translateY(0);
-    }
-
-    .mobileTips {
-        width: 94%;
-        margin-left: 3%;
-        margin-top: 10px;
-        padding: 10px;
-        color: #333;
-        background: #F8F8F8;
-        border-radius: 6px;
-        border: 1px solid #eee;
-
-        p {
-            font-size: 13px;
-            line-height: 18px;
-            text-indent: 2em;
-            text-align: justify;
-
-            a {
-                color: blue;
-                text-decoration: underline;
-            }
-
-            b {
-                color: #F45757;
-            }
-        }
-    }
-
-    .form {
-        width: 94%;
-        margin-left: 3%;
-        margin-top: 10px;
+        height: 36px;
+        margin: 5px 0;
         padding: 0 10px;
-        color: #222;
-
-        label {
-            font-size: 14px;
-            line-height: 20px;
-        }
-
-        input[type="text"] {
-            width: 100%;
-            height: 36px;
-            margin: 5px 0;
-            padding: 0 10px;
-            font-size: 14px;
-            line-height: 36px;
-            background: #F6F6F6;
-            border-radius: 4px;
-            border: 1px solid #ccc;
-        }
-
-        input[type="radio"] {
-            width: 16px;
-            height: 16px;
-            margin: 10px 0;
-            background: #F6F6F6;
-            border-radius: 4px;
-            border: 1px solid #ccc;
-            -webkit-appearance: radio;
-        }
-
-        button {
-            width: 100%;
-            height: 36px;
-            margin: 10px 0;
-            color: #FFF;
-            font-size: 16px;
-            line-height: 34px;
-            background: #46d14c;
-            border: 1px solid #45b94a;
-            border-radius: 4px;
-
-            &:active {
-                background: #45b94a;
-            }
-        }
+        font-size: 14px;
+        line-height: 36px;
+        background: #F6F6F6;
+        border-radius: 4px;
+        border: 1px solid #ccc;
     }
 
-    .pop {
-        position: fixed;
-        width: 80%;
-        height: 80%;
-        left: 10%;
-        top: 10%;
-        background: #FFF;
-        box-shadow: 0 5px 10px rgba(0,0,0,.3);
-        border-radius: 6px;
-        z-index: 11;
-        overflow: auto;
-
-        h3 {
-            color: #FFF;
-            font-size: 16px;
-            text-align: center;
-            line-height: 40px;
-            background: #F45757;
-            border-radius: 6px 6px 0 0;
-        }
-
-        .list {
-            float: left;
-            width: 80%;
-            margin-top: 20px;
-            margin-left: 10%;
-            display: flex;
-            font-size: 15px;
-            line-height: 30px;
-            flex-wrap: wrap;
-            align-items: center;
-
-            .key {
-                width: 45%;
-                padding: 0 5px;
-                color: #111;
-                font-weight: bold;
-                text-align: right;
-            }
-
-            .value {
-                width: 55%;
-                font-size: 14px;
-                color: #444;
-                text-align: left;
-                padding: 0 5px;
-            }
-
-            .zkz {
-                font-size: 12px;
-            }
-
-            .sum {
-                color: #F45757;
-                font-size: 16px;
-                font-weight: bold;
-            }
-        }
-
-        .tips {
-            float: left;
-            width: 100%;
-            padding: 10px;
-
-            p {
-                color: #aaa;
-                font-size: 12px;
-            }
-        }
-
-        button {
-            float: left;
-            width: 80%;
-            height: 36px;
-            margin-left: 10%;
-            color: #888;
-            font-size: 16px;
-            line-height: 34px;
-            background: #FFF;
-            border: 1px solid #eee;
-            border-radius: 4px;
-
-            &:active {
-                background: #eee;
-            }
-        }
-        
+    input[type="radio"] {
+        width: 16px;
+        height: 16px;
+        margin: 10px 0;
+        background: #F6F6F6;
+        border-radius: 4px;
+        border: 1px solid #ccc;
+        -webkit-appearance: radio;
     }
 
-    .popMask {
-        position: fixed;
-        height: 100%;
+    button {
         width: 100%;
-        background: rgba(0,0,0,.7);
-        z-index: 10;
-    }
+        height: 36px;
+        margin: 10px 0;
+        color: #FFF;
+        font-size: 16px;
+        line-height: 34px;
+        background: #46d14c;
+        border: 1px solid #45b94a;
+        border-radius: 4px;
 
-    footer {
-        margin-top: 10px;
-        width: 100%;
-        text-align: center;
-
-        p {
-            font-size: 12px;
-            color: #aaa;
-
-            a {
-                color: #888;
-                text-decoration: underline;
-            }
+        &:active {
+            background: #45b94a;
         }
     }
 }
+
+.pop {
+    position: fixed;
+    width: 80%;
+    height: 80%;
+    left: 10%;
+    top: 10%;
+    background: #FFF;
+    box-shadow: 0 5px 10px rgba(0,0,0,.3);
+    border-radius: 6px;
+    z-index: 11;
+    overflow: auto;
+
+    h3 {
+        color: #FFF;
+        font-size: 16px;
+        text-align: center;
+        line-height: 40px;
+        background: #F45757;
+        border-radius: 6px 6px 0 0;
+    }
+
+    .list {
+        float: left;
+        width: 80%;
+        margin-top: 20px;
+        margin-left: 10%;
+        display: flex;
+        font-size: 15px;
+        line-height: 30px;
+        flex-wrap: wrap;
+        align-items: center;
+
+        .key {
+            width: 45%;
+            padding: 0 5px;
+            color: #111;
+            font-weight: bold;
+            text-align: right;
+        }
+
+        .value {
+            width: 55%;
+            font-size: 14px;
+            color: #444;
+            text-align: left;
+            padding: 0 5px;
+        }
+
+        .zkz {
+            font-size: 12px;
+        }
+
+        .sum {
+            color: #F45757;
+            font-size: 16px;
+            font-weight: bold;
+        }
+    }
+
+    .tips {
+        float: left;
+        width: 100%;
+        padding: 10px;
+
+        p {
+            color: #aaa;
+            font-size: 12px;
+        }
+    }
+
+    button {
+        float: left;
+        width: 80%;
+        height: 36px;
+        margin-left: 10%;
+        color: #888;
+        font-size: 16px;
+        line-height: 34px;
+        background: #FFF;
+        border: 1px solid #eee;
+        border-radius: 4px;
+
+        &:active {
+            background: #eee;
+        }
+    }
+    
+}
+
+.popMask {
+    position: fixed;
+    height: 100%;
+    width: 100%;
+    background: rgba(0,0,0,.7);
+    z-index: 10;
+}
+
+footer {
+    margin-top: 10px;
+    width: 100%;
+    text-align: center;
+
+    p {
+        font-size: 12px;
+        color: #aaa;
+
+        a {
+            color: #888;
+            text-decoration: underline;
+        }
+    }
+}
+
 </style>
